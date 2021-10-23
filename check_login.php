6257f2aead25475be11487310c55cfe1e358e33a
lacakp <lacakp.contact@gmail.com>
2021-10-23 21:46:13 +0700

    update 1.0

--- check_login.php
+++ check_login.php
@@ -12,7 +12,29 @@
 	$objResult =$objQuery->fetch_assoc();
 	if(!$objResult)
 	{
-			echo "Username and Password Incorrect!";
+			echo '
+			<div class="bg"> </div>
+				<div class="container" id="app">
+				<div class="row justify-content-center align-items-center vh-100">
+					<div class="col-md-6">
+					<div class="card py-3 p-lg-5 shadow">
+						<div class="card-title">
+						<img src="assets/images/panda-pixel.png" width="300px" class="img-fluid d-block mx-auto" alt="logo">
+						</div>
+						<div class="card-body">
+								<h6 class="text-danger text-center">Oops! Looks like you have entered the wrong username of password. Please check your login details and try again.</h6>
+									<form name="form1" method="post" action="login.php">
+									<br>
+									<center>
+									<button class="btn btn-primary">Login Page </button>
+									</center>
+								</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			';
 	}
 	else
 	{
@@ -32,3 +54,13 @@
 	}
 	
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+</html>
--- save_profile.php
+++ save_profile.php
@@ -17,16 +17,77 @@
 	
 	$objQuery = $conn->query($strSQL);
 
-	echo "Save Completed!<br>";		
+			
 	
 	if($_SESSION["Status"] == "ADMIN")
 	{
-		echo "<br> Go to <a href='admin_page.php'>Admin page</a>";
+		echo '
+		<div class="bg"> </div>
+			<div class="container" id="app">
+			<div class="row justify-content-center align-items-center vh-100">
+				<div class="col-md-6">
+				<div class="card py-3 p-lg-5 shadow">
+					<div class="card-title">
+					<img src="assets/images/panda-pixel.png" width="300px" class="img-fluid d-block mx-auto" alt="logo">
+					</div>
+					<div class="card-body">
+							<h1 class="text-center">Go to Admin Page</h1>
+						<form name="form1" method="post" action="admin_page.php">
+								<br>
+								<center>
+								<button class="btn btn-primary"> Admin Page </button>
+								</center>
+
+							</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		';
 	}
 	else
 	{
-		echo "<br> Go to <a href='user_page.php'>User page</a>";
+		echo '
+		<div class="bg"> </div>
+			<div class="container" id="app">
+			<div class="row justify-content-center align-items-center vh-100">
+				<div class="col-md-6">
+				<div class="card py-3 p-lg-5 shadow">
+					<div class="card-title">
+					<img src="assets/images/panda-pixel.png" width="300px" class="img-fluid d-block mx-auto" alt="logo">
+					</div>
+					<div class="card-body">
+							<h1 class="text-center">Go to User Page</h1>
+						<form name="form1" method="post" action="user_page.php">
+								<br>
+								<center>
+								<button class="btn btn-primary"> User Page </button>
+								</center>
+
+							</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		';
 	}
-	
-	
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Save Completed</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+	<h1>
+		"Save Completed!<br>
+	</h1>
+</body>
+</html>
